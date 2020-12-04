30af823126173cd1e0f095e96d727e99e15cfd6c
Mallik Ebadat <31974569+MK-IB@users.noreply.github.com>
2020-12-04 11:27:00 +0530

    Add files via upload

---
+++ RELAY_LIGHT_CONTROL/RELAY_LIGHT_CONTROL.ino
@@ -0,0 +1,25 @@
+int REL = 8;
+char input;
+void setup() {
+  Serial.begin(9600);
+  pinMode(REL, OUTPUT);
+
+}
+
+void loop() {
+  if(Serial.available() > 0)
+  {
+      input = Serial.read();
+    if(input == '1')
+    {
+      digitalWrite(REL, HIGH);
+     Serial.write("Switched ON");
+    }
+    if(input == '2')
+    {
+      digitalWrite(REL, LOW);
+      Serial.write("Switched OFF");
+    }
+  }
+   //digitalWrite(REL, HIGH);
+}
