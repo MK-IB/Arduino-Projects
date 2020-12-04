ee6bd7ac32f60c0f2af243fb812864fa101144f5
Mallik Ebadat <31974569+MK-IB@users.noreply.github.com>
2020-12-04 11:27:33 +0530

    Add files via upload

---
+++ Ultrasonic_Sensor/Ultrasonic_Sensor.ino
@@ -0,0 +1,25 @@
+const int trigPin = 9;
+const int echoPin = 10;
+long dur;
+int dist;
+void setup() {
+ pinMode(13, OUTPUT);
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+ Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  dur = pulseIn(echoPin, HIGH);
+  dist = dur * 0.034/2;
+  Serial.print(dist);
+  //Serial.print("\n");
+  delay(1000);
+}
